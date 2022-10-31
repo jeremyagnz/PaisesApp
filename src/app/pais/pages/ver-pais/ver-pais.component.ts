f822a24168fc25d1bf47d2c1063d2c5353550ffe
jeremyagnz <jeremyagnz@gmail.com>
2022-10-31 16:59:09 -0400

    Using Switchmap from js to simplify two subscribe

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,31 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { switchMap } from 'rxjs';
+import { switchMap, tap } from 'rxjs/operators';
+
 import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-ver-pais',
   templateUrl: './ver-pais.component.html',
-  styleUrls: ['./ver-pais.component.css'],
+  styles: [
+  ]
 })
 export class VerPaisComponent implements OnInit {
+
+  pais!: Country[];
+
   constructor(
     private activatedRoute: ActivatedRoute,
     private paisService: PaisService
-  ) {}
+    ) { }
 
   ngOnInit(): void {
-    this.activatedRoute.params
-      .pipe(switchMap(({ id }) => this.paisService.getPaisPorId(id)))
-      .subscribe((resp) => {});
-    /*  this.activatedRoute.params.subscribe(({ id }) => {
-      console.log(id);
 
-      this.paisService.getPaisPorId(id)
-      .subscribe (pais => {
-        id = pais;
-        console.log(pais);
-      })
-    }); */
+    this.activatedRoute.params
+      .pipe(
+        switchMap( ({ id }) => this.paisService.getPaisPorAlpha( id )  ),
+        tap( console.log )
+      )
+      .subscribe( pais => this.pais = pais );
+
   }
+
 }
