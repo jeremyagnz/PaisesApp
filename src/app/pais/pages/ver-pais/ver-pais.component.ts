4d3f2207812ecb417c8a8164d985bf52321a95db
Jeremy Arias <35277430+jeremyagnz@users.noreply.github.com>
2022-10-31 15:27:44 -0400

    Consumin Api from Pais/Code and using ActivatedRoute (#11)

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-ver-pais',
   templateUrl: './ver-pais.component.html',
-  styleUrls: ['./ver-pais.component.css']
+  styleUrls: ['./ver-pais.component.css'],
 })
 export class VerPaisComponent implements OnInit {
-
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private paisService: PaisService
+  ) {}
 
   ngOnInit(): void {
-  }
+    this.activatedRoute.params.subscribe(({ id }) => {
+      console.log(id);
 
+      this.paisService.getPaisPorId(id)
+      .subscribe (pais => {
+        id = pais;
+        console.log(pais);
+      })
+    });
+  }
 }
