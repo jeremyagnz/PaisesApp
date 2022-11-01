41f2b50b513ac2e20856231cbe87dff7d8d814ac
jeremyagnz <jeremyagnz@gmail.com>
2022-11-01 19:25:23 -0400

    Adding regiones and consuming api in region Component

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
-  styleUrls: ['./por-region.component.css']
+  styleUrls: ['./por-region.component.css'],
 })
-export class PorRegionComponent implements OnInit {
+export class PorRegionComponent {
+  regiones: string[] = [
+    'EU',
+    'EFTA',
+    'CARICOM',
+    'PA',
+    'AU',
+    'USAN',
+    'EEU',
+    'AL',
+    'ASEAN',
+    'CAIS',
+    'CEFTA',
+    'NAFTA',
+    'SAARC',
+  ];
+  regionActiva: string = '';
+  paises: Country[] = [];
 
-  constructor() { }
+  constructor(private paisService:PaisService) {}
 
-  ngOnInit(): void {
+  getClaseCSS(region: string) {
+    return region === this.regionActiva
+      ? 'btn btn-primary mr-2'
+      : 'btn btn-outline-primary mr-2';
   }
 
+  activarRegion(region: string) {
+    if(region === this.regionActiva){return; }
+    this.regionActiva = region;
+    this.paises = [];
+
+    this.paisService.buscarPorRegion(region)
+        .subscribe( paises => this.paises = paises)
+  }
 }
